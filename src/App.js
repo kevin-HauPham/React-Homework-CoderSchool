08d661301795743a836388618703264f3807af22
bo^` <bo@192.168.2.7>
2024-09-10 13:58:02 +0700

    add product and app's features

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./routes";
+import { AuthProvider } from "./contexts/AuthContext";
+import ThemeProvider from "./contexts/ThemeProvider";
 
 function App() {
-  return <div></div>;
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <ThemeProvider>
+          <Router />
+        </ThemeProvider>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
 export default App;
